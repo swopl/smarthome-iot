3aeb5d348f78480923cf92fcaeb08355e389a0ef
swopl <modaik5@gmail.com>
2024-01-22 21:10:55 +0100

    feat: Also log if it happens

--- web/cmd/alarm_handling.go
+++ web/cmd/alarm_handling.go
@@ -47,6 +47,7 @@ func (pc *PersonCounter) handleNewPerson(client mqtt.Client, msg mqtt.Message) {
 func publishPeopleCount(client mqtt.Client, count int32) {
 	// TODO: check if this works, sending only i32...
 	if count < 0 {
+		log.Println("Negative count for people somehow:", count)
 		count = 0
 	}
 	client.Publish("PeopleCount", 2, true, count)
