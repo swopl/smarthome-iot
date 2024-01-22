8fe8014231fd10e4c8fbb3cf25e05e1e5ac9e388
swopl <modaik5@gmail.com>
2024-01-22 19:04:49 +0100

    feat: Simulate UDS from csv

--- pi/simulators/uds.py
+++ pi/simulators/uds.py
@@ -1,11 +1,20 @@
+import csv
 import time
 import random
 
 
 def run_uds_simulator(callback, stop_event, code):
+    simulated_values = []
+    with open("./pre_simulated/uds_entering.csv", "r") as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            simulated_values.append([float(value) for value in row])
+    i = 0
     while True:
         delay = random.randint(7, 14) / 5.0
         time.sleep(delay)
-        callback(code, random.randint(10, 1000) / 100)
+        callback(code, simulated_values[i])
+        if (i + 1) < len(simulated_values):
+            i = i + 1
         if stop_event.is_set():
             break
