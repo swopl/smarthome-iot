e9cd8262ea9260aeca43a65dce458a5e12dc1266
swopl <modaik5@gmail.com>
2024-01-25 03:20:16 +0100

    fix: UDS sim wrong format

--- pi/simulators/uds.py
+++ pi/simulators/uds.py
@@ -8,7 +8,7 @@ def run_uds_simulator(callback, stop_event, code):
     with open("./pre_simulated/uds_entering.csv", "r") as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
-            simulated_values.append([float(value) for value in row])
+            simulated_values.extend([float(value) for value in row])
     i = 0
     while True:
         delay = random.randint(7, 14) / 5.0
